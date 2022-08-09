a99cd70d6e56250c868e419ad331b9137d12e652
yogita780 <yogita.balyan_cs19@gla.ac.in>
2022-08-09 12:55:57 +0530

    final commit

---
+++ day-6/myscript.js
@@ -0,0 +1,8 @@
+// console.log("welcome");
+// var a=10;
+// var b = 20;
+// var c = a+b;
+// console.log("sum"  +c);
+
+var date =  new Date();
+var dateF = "Today Date"+ date.getDay + "/" + date.getMonth + "/" + 
