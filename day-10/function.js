8c0ce4009bd5f21c6fdf4b378946d3bddd5c2358
yogita780 <yogita.balyan_cs19@gla.ac.in>
2022-08-17 15:29:10 +0530

    final commit

---
+++ day-10/function.js
@@ -0,0 +1,17 @@
+// var obj = {
+//     firstName : yogita,
+//     lastName: singh,
+//     fullName: function(){
+//         return this.firstName + ""+ this.lastName;
+//     },
+// };
+// console.log(obj.fullName());
+
+// var person1 = {
+//     firstName : yug,
+//     lastName: singh,
+// };
+// console.log(obj.fullName
+//     .call(person1));
+
+
