dc729c36876e4e59721c20dba9e564fbbd2faaea
yogita780 <yogita.balyan_cs19@gla.ac.in>
2022-08-29 18:11:37 +0530

    commit

---
+++ gaming/sketch.js
@@ -0,0 +1,165 @@
+// Global Scope
+var bg, bgImage;
+var mario, mario_running;
+var ground;
+var brickImage,bricksGroup ;
+var coinsImage,coinsGroup, coin_running ;
+var coinsScore =0;
+var coinSound;
+var jumpSound;
+var bgSound;
+var mush_running;
+var tur_running;
+
+// Load Assets
+function preload() {
+  bgImage = loadImage("./images/bgnew .jpg");
+ 
+ 
+  mario_running = loadAnimation(
+    "./images/mar1.png",
+    "./images/mar2.png",
+    "./images/mar3.png",
+    "./images/mar4.png",
+    "./images/mar5.png",
+    "./images/mar6.png",
+    "./images/mar7.png"
+  );
+  brickImage = loadImage("./images/brick.png");
+  coinsImage = loadAnimation(
+    "./images/con1.png",
+    "./images/con2.png",
+    "./images/con3.png",
+    "./images/con4.png",
+    "./images/con5.png",
+    "./images/con6.png",
+  );
+  mush_running = loadAnimation(
+    "./images/mush1.png",
+    "./images/mush2.png",
+    "./images/mush3.png",
+    "./images/mush4.png",
+    "./images/mush5.png",
+    "./images/mush6.png",
+    
+  );
+  tur_running = loadAnimation(
+    "./images/tur1.png",
+    "./images/tur2.png",
+    "./images/tur3.png",
+    "./images/tur4.png",
+    "./images/tur5.png",
+    
+  );
+  coinSound = loadSound("./sound/coinSound.mp3");
+  jumpSound = loadSound("./sound/sounds_jump.mp3");
+  bgSound = loadSound("./sound/mario-bg.mp3");
+}
+
+// create basic Scaleton with their required credentials
+function setup() {
+  // Create Canvas
+  createCanvas(1000, 600);
+  bgSound.play();
+  bgSound.loop();
+
+  // create Objects
+  bg = createSprite(600, 300, 150, 50);
+  mario = createSprite(200, 520, 150, 50);
+
+  // Add pictures on Objects
+  bg.addImage(bgImage);
+  mario.addAnimation("running", mario_running);
+  // coin.addAnimation("running", coin_running);
+
+  // Scale Objects
+  bg.scale = 0.5;
+  mario.scale = 0.2;
+
+  // create Ground
+  ground = createSprite(200, 580, 400, 10);
+  bricksGroup = new Group();
+  coinsGroup = new Group();
+}
+
+// Used to redraw the Objects on the canvas
+function draw() {
+  // Make background move and repeate
+  bg.velocityX = -5;
+  if (bg.x < 100) bg.x = bg.width / 4;
+
+  // mario Fly
+  if (keyDown("space")){
+
+  mario.velocityY = -10;
+  jumpSound.play();
+  }
+  // add Gravity
+  mario.velocityY = mario.velocityY + 0.5;
+
+  // mario stuck on ground
+  mario.collide(ground);
+  ground.visible = false;
+
+  //call generateBricks
+  generateBricks();
+
+
+  for (var i = 0; i < bricksGroup.length; i++) {
+    var temp = bricksGroup.get(i);
+    if (mario.isTouching(temp)) {
+      mario.collide(temp);
+    }
+  }
+  if (mario.x < 200) {
+    mario.x = 200;
+  }
+  if (mario.y < 50) {
+    mario.y = 50;
+  }
+  generateCoins();
+
+  for (var i = 0; i < coinsGroup.length; i++) {
+    var temp = coinsGroup.get(i);
+    if (mario.isTouching(temp)) {
+     temp.destroy();
+     coinSound.play();
+     coinsScore++;
+     temp = null;
+    }
+  }
+
+  // Redraw Objects
+  drawSprites();
+
+
+  textSize(20);
+  fill("brown");
+  text("coin collected :" + coinsScore ,500,50 );
+}
+function generateBricks() {
+  if (frameCount % 80 == 0) {
+    console.log(frameCount);
+
+    var brick = createSprite(1200, 100, 40, 10);
+    brick.y = random(50, 450);
+    brick.addImage(brickImage);
+    brick.scale = 0.5;
+    brick.velocityX = -5;
+    brick.lifetime = 250;
+    bricksGroup.add(brick);
+  }
+}
+function generateCoins() {
+  if (frameCount % 50 == 0) {
+    // console.log(frameCount);
+
+    var coin = createSprite(1200, 100, 40, 10);
+    coin.y = random(50, 450);
+    coin.addAnimation("rotate",coinsImage);
+    coin.scale = 0.1;
+    coin.velocityX = -5;
+    coin.lifetime = 250;
+    coinsGroup.add(coin);
+  }
+}
