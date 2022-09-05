6d3cb318c92a092f2bb6be4894363c6dd6821526
yogita780 <yogita.balyan_cs19@gla.ac.in>
2022-09-05 09:44:03 +0530

    commit

---
+++ day-19/bundle/src/index.js
@@ -0,0 +1,7 @@
+const lib = require('./library.js');
+const lodash = require('lodash');
+console.log(lib.add(5,10));
+
+console.log(lodash.upperCase("ballu "));
+console.log(lodash.add(233444,535));
+
